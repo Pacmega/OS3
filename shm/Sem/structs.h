6ce386c5f14be913a237f6620d0096f7d183c8a2
Pacmega <bas.witters@hotmail.com>
2017-12-07 15:32:19 +0100

    Hmm... Forgot to commit this name change

---
+++ shm/Sem/structs.h
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+
+typedef struct
+{
+	int 	value;
+	char* 	pronunciation;
+} number;
+
+typedef struct
+{
+	char * 	sharedMem;
+	sem_t * semaphore;
+} multithreading;
