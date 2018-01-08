111acaa6c8d88894f2df9a129b895266e6e80510
Pacmega <bas.witters@hotmail.com>
2018-01-08 17:00:38 +0100

    Is this working?

--- finalApp/lib/structs.h
+++ finalApp/lib/structs.h
@@ -6,12 +6,23 @@
 
 typedef struct
 {
-    unsigned char group1;       // D-Pad, start and back buttons and stick presses
-    unsigned char group2;       // Shoulder buttons, A/B/X/Y and Xbox logo
-    unsigned char leftTrigger;  // Left trigger
-    unsigned char rightTrigger; // Right trigger
-    int           leftStick;    // Left stick
-    int           rightStick;   // Right stick
+    unsigned char group1Input;       // D-Pad, start and back buttons and stick presses
+    unsigned char group2Input;       // Shoulder buttons, A/B/X/Y and Xbox logo
+    unsigned char leftTriggerInput;  // Left trigger
+    unsigned char rightTriggerInput; // Right trigger
+    int           leftStickInput;    // Left stick
+    int           rightStickInput;   // Right stick
+    /*
+    printf("Input report\n");
+    printf("Message type\t\t%d\n", inputReport[messageType]);
+    printf("Packet size (B)\t\t%d\n", inputReport[packetSize]);
+    printf("D-Pad + button group 1\t%d\n", inputReport[inputGroup1]);
+    printf("(Start/Back & stick press)\n");
+    printf("Button group 2\t\t%d\n", inputReport[inputGroup2]);
+    printf("(Shoulders, A/B/X/Y, Xbox logo)\n");
+    printf("Left trigger\t\t%d\n", inputReport[leftTrigger]);
+    printf("Right trigger\t\t%d\n", inputReport[rightTrigger]);
+    */
 } inputStruct;
 
 typedef struct 
