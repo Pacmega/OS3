25460ce742d02b36030190e2105a7b6ac88d5498
Pacmega <bas.witters@hotmail.com>
2018-01-08 11:53:56 +0100

    Added separate file for defines

---
+++ finalApp/lib/defines.h
@@ -0,0 +1,9 @@
+#ifndef DEFINES
+#define DEFINES
+
+#define writeSemName "/itemsToWrite"
+#define readSemName "/itemsToRead"
+#define sharedMemName "OSUSBsharedmem"
+#define mqName "/_mq_mq_" // TODO: a good name?
+
+#endif
