19d9b3fec5630c5d67a77d469162d0c7f989e97d
Joran van Teeffelen <joranvt@gmail.com>
2018-01-08 14:40:28 +0100

    Create defines.h

---
+++ finalApp/lib/defines.h
@@ -0,0 +1,11 @@
+#ifndef DEFINES
+#define DEFINES
+
+#define writeSemName "/itemsToWrite"
+#define readSemName "/itemsToRead"
+#define sharedMemName "OSUSBsharedmem"
+#define mqName "/_mq_mq_" // TODO: a good name?
+
+#define deadzone 10
+
+#endif
