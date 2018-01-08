b61c2f12b6967d49b792e5517c2100d89e664cc0
Pacmega <bas.witters@hotmail.com>
2018-01-08 12:07:09 +0100

    Experimenting to learn MQ's

--- finalApp/lib/semshm.h
+++ finalApp/lib/semshm.h
@@ -3,10 +3,6 @@
 
 #include <semaphore.h>
 
-#define writeSemName  /itemsToWrite
-#define readSemName   /itemsToRead
-#define sharedMemName OSUSBsharedMem
-
 char * my_shm_create (int size, char* memoryName);
 char * my_shm_open (int size, char* memoryName);
 sem_t * my_sem_open (char* semaphoreName);
